a52f4397ab84ca3f0414dd046fb6a6b526f74957
alexey smirnov <alexey.ana.smirnov@megafon.ru>
2021-03-11 02:02:17 +0400

    lesson6 HW

---
+++ lesson6/gb_parse/loaders.py
@@ -0,0 +1,7 @@
+from scrapy.loader import ItemLoader
+from urllib.parse import urljoin
+from scrapy import Selector
+from itemloaders.processors import TakeFirst, MapCompose
+#from .items import GbAutoYoulaItem
+
+
---
+++ lesson6/gb_parse/spiders/instagram.py
@@ -0,0 +1,43 @@
+import json
+
+import scrapy
+
+
+class InstagramSpider(scrapy.Spider):
+    name = "instagram"
+    allowed_domains = ["www.instagram.com"]
+    start_urls = ["https://www.instagram.com/"]
+    _login_url = "https://www.instagram.com/accounts/login/ajax/"
+    _tags_path = "/explore/tags/"
+
+    def __init__(self, login, password, tags, *args, **kwargs):
+        self.login = login
+        self.password = password
+        self.tags = tags
+        super().__init__(*args, **kwargs)
+
+    def parse(self, response):
+        try:
+            js_data = self.js_data_extract(response)
+            yield scrapy.FormRequest(
+                self._login_url,
+                method="POST",
+                callback=self.parse,
+                formdata={"username": self.login, "enc_password": self.password,},
+                headers={"X-CSRFToken": js_data["config"]["csrf_token"]},
+            )
+        except AttributeError as e:
+            print(e)
+            for tag_name in self.tags:
+                yield response.follow(
+                    f"{self._tags_path}{tag_name}/", callback=self.tag_page_parse
+                )
+
+    def tag_page_parse(self, response):
+        print(1)
+
+    def js_data_extract(self, response):
+        script = response.xpath(
+            "//script[contains(text(), 'window._sharedData =')]/text()"
+        ).extract_first()
+        return json.loads(script.replace("window._sharedData = ", "")[:-1])
